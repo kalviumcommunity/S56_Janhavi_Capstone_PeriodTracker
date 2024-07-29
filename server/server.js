734b830246ef02f762eb0c3cc703363c11612a42
Janhavi003 <144115237+Janhavi003@users.noreply.github.com>
2024-07-29 14:49:02 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const app = express();
 const cors = require('cors');
 
 const corsOptions = {
-  origin : 'http://localhost:5173',
+  origin : 'https://msfemmigo.netlify.app',
   methods : ["GET","POST","PUT","DELETE"],
   allowedHeaders : ["Content-Type","Authorization"],
   credentials : true
